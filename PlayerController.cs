82c8498582f0564817b3b055fa4e119889c14c14
yooan321 <yooanean.99@gmail.com>
2025-07-02 21:16:07 +0100

    NPC dialogue

--- PlayerController.cs
+++ PlayerController.cs
@@ -3,114 +3,133 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
 public class PlayerController : MonoBehaviour
 {
-  // Variables related to player character movement
-  public InputAction MoveAction;
-  Rigidbody2D rigidbody2d;
-  Vector2 move;
-  public float speed = 3.0f;
+    // Variables related to player character movement
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public float speed = 3.0f;
+
+    // Variables related to the health system
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    // Variables related to temporary invincibility
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+
+    // Variables related to Animation
+    Animator animator;
+    Vector2 moveDirection = new Vector2(1, 0);
+
+    // // Variables related to Projectile
+    public GameObject projectilePrefab;
 
 
-  // Variables related to the health system
-  public int maxHealth = 5;
-  int currentHealth;
-  public int health { get { return currentHealth; }}
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+    }
 
 
-  // Variables related to temporary invincibility
-  public float timeInvincible = 2.0f;
-  bool isInvincible;
-  float damageCooldown;
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log(move);
 
 
-   // Variables related to animation
-  Animator animator;
-  Vector2 moveDirection = new Vector2(1,0);
-
-
-  // Variables related to projectiles
-  public GameObject projectilePrefab;
-
-
-
-  // Start is called before the first frame update
-  void Start()
-  {
-     MoveAction.Enable();
-     rigidbody2d = GetComponent<Rigidbody2D>();
-     animator = GetComponent<Animator>();
-
-     currentHealth = maxHealth;
-  }
- 
-  // Update is called once per frame
-  void Update()
-  {
-     move = MoveAction.ReadValue<Vector2>();
-
-
-      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
         {
-           moveDirection.Set(move.x, move.y);
-           moveDirection.Normalize();
+            moveDirection.Set(move.x, move.y);
+            moveDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", moveDirection.x);
+        animator.SetFloat("Look Y", moveDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
         }
 
 
-     animator.SetFloat("Look X", moveDirection.x);
-     animator.SetFloat("Look Y", moveDirection.y);
-     animator.SetFloat("Speed", move.magnitude);
-
-
-     if (isInvincible)
-       {
-           damageCooldown -= Time.deltaTime;
-           if (damageCooldown < 0)
-               isInvincible = false;
-       }
-
-
-     if(Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
         {
-           Launch();
+            Launch();
         }
 
-   }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+           FindFriend();
+        }
+    }
 
 
-// FixedUpdate has the same call rate as the physics system
-  void FixedUpdate()
-  {
-     Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
-     rigidbody2d.MovePosition(position);
-  }
 
 
-  public void ChangeHealth (int amount)
-  {
-     if (amount < 0)
-       {
-           if (isInvincible)
-               return;
-          
-           isInvincible = true;
-           damageCooldown = timeInvincible;
-           animator.SetTrigger("Hit");
-       }
+    // FixedUpdate has the same call rate as the physics system 
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
 
 
-     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-     UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
-  }
 
-  void Launch()
-  {
-     GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
-     Projectile projectile = projectileObject.GetComponent<Projectile>();
-     projectile.Launch(moveDirection, 300);
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+            animator.SetTrigger("Hit");
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
+    }
 
 
-     animator.SetTrigger("Launch");
-  }
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(moveDirection, 300);
+        animator.SetTrigger("Launch");
+    }
 
+
+    void FindFriend()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, moveDirection, 1.5f, LayerMask.GetMask("NPC"));
+        if (hit.collider != null)
+        {
+            NonPlayerCharacter character = hit.collider.GetComponent<NonPlayerCharacter>();
+            if (character != null)
+            {
+                UIHandler.instance.DisplayDialogue();
+            }
+        }
+    }
+    
+    
 }
--- UIHandler.cs
+++ UIHandler.cs
@@ -1,27 +1,67 @@
-using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UIElements;
 
-
-
-public class UIHandle : MonoBehaviour
+public class UIHandler : MonoBehaviour
 {
-    public static UIHandle instance { get; private set; }
-    private VisualElement m_Healthbar;
-    private void Awake()
-    {
+   private VisualElement m_Healthbar;
+   public static UIHandler instance { get; private set; }
 
-        instance = this;  
-    } 
-    void Start()
-    {
-        UIDocument uIDocument = GetComponent<UIDocument>();
-        m_Healthbar = uIDocument.rootVisualElement.Q<VisualElement>("HealthBar");
-        SetHealthValue(1.0f);
-    }
-    public void SetHealthValue(float percentage)
-    {
-        m_Healthbar.style.width = Length.Percent(100 * percentage); 
-    }
+   // UI dialogue window variables
+   public float displayTime = 4.0f;
+   private VisualElement m_NonPlayerDialogue;
+   private float m_TimerDisplay;
+
+
+   // Awake is called when the script instance is being loaded (in this situation, when the game scene loads)
+   private void Awake()
+   {
+       instance = this;
+   }
+
+   // Start is called before the first frame update
+   private void Start()
+   {
+       UIDocument uiDocument = GetComponent<UIDocument>();
+       m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
+       SetHealthValue(1.0f);
+
+
+       m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
+       m_NonPlayerDialogue.style.display = DisplayStyle.None;
+       m_TimerDisplay = -1.0f;
+
+
+   }
+
+
+
+   private void Update()
+   {
+       if (m_TimerDisplay > 0)
+       {
+           m_TimerDisplay -= Time.deltaTime;
+           if (m_TimerDisplay < 0)
+           {
+               m_NonPlayerDialogue.style.display = DisplayStyle.None;
+           }
+
+
+       }
+   }
+
+
+   public void SetHealthValue(float percentage)
+   {
+       m_Healthbar.style.width = Length.Percent(100 * percentage);
+   }
+   
+   
+   public void DisplayDialogue()
+   {
+   m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
+   m_TimerDisplay = displayTime;
+   }
+
 }
