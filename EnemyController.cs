c0a3ad619374d8198e3ef9a0dad75065eebbc52a
yooan321 <yooanean.99@gmail.com>
2025-07-02 21:07:12 +0100

    New player controller and enemy controller scripts

--- EnemyController.cs
+++ EnemyController.cs
@@ -2,64 +2,74 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class EnemyController : MonoBehaviour
 {
-    // Public variables
-    public float speed;
-    public bool vertical;
-    public float changeTime = 3.0f;
-
-    // Private variables
-    Rigidbody2D rigidbody2d;
-    float timer;
-    int direction = 1;
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        timer = changeTime;
-    }
-
-    // Update is called every frame
-    void Update()
-    {
-
-        timer -= Time.deltaTime;
+   // Public variables
+   public float speed;
+   public bool vertical;
+   public float changeTime = 3.0f;
+  
+   // Private variables
+   Rigidbody2D rigidbody2d;
+   Animator animator;
+   float timer;
+   int direction = 1;
 
 
-        if (timer < 0)
-        {
-            direction = -direction;
-            timer = changeTime;
-        }
-    }
-
-    // FixedUpdate has the same call rate as the physics system
-    void FixedUpdate()
-    {
-        Vector2 position = rigidbody2d.position;
-
-        if (vertical)
-        {
-            position.y = position.y + speed * direction * Time.deltaTime;
-        }
-        else
-        {
-            position.x = position.x + speed * direction * Time.deltaTime;
-        }
-
-
-        rigidbody2d.MovePosition(position);
-    }
-  void OnTriggerEnter2D(Collider2D other)
+   // Start is called before the first frame update
+   void Start()
    {
-    PlayerController player = other.gameObject.GetComponent<PlayerController>();
-    if (player != null)
+       rigidbody2d = GetComponent<Rigidbody2D>();
+       animator = GetComponent<Animator>();
+       timer = changeTime;
+      
+   }
+   
+   void Update()
+   {
+   timer -= Time.deltaTime;
+
+
+      if (timer < 0)
+      {
+           direction = -direction;
+           timer = changeTime;
+      }
+   }
+
+
+  // FixedUpdate has the same call rate as the physics system
+  void FixedUpdate()
+  {
+       Vector2 position = rigidbody2d.position;
+     
+       if (vertical)
+       {
+           position.y = position.y + speed * direction * Time.deltaTime;
+           animator.SetFloat("Move X", 0);
+           animator.SetFloat("Move Y", direction);
+       }
+       else
+       {
+           position.x = position.x + speed * direction * Time.deltaTime;
+           animator.SetFloat("Move X", direction);
+           animator.SetFloat("Move Y", 0);
+       }
+
+
+       rigidbody2d.MovePosition(position);
+  }
+
+
+   void OnTriggerEnter2D(Collider2D other)
+   {
+       PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+
+       if (player != null)
        {
            player.ChangeHealth(-1);
        }
    }
+
 }
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,16 +10,23 @@ public class PlayerController : MonoBehaviour
   Rigidbody2D rigidbody2d;
   Vector2 move;
   public float speed = 3.0f;
-   // Variables related to temporary invincibility
-  public float timeInvincible = 2.0f;
-   bool isInvincible;
-   float damageCooldown;
 
 
   // Variables related to the health system
-   public int maxHealth = 5;
-  public int health { get { return currentHealth; }}
+  public int maxHealth = 5;
   int currentHealth;
+  public int health { get { return currentHealth; }}
+
+
+  // Variables related to temporary invincibility
+  public float timeInvincible = 2.0f;
+  bool isInvincible;
+  float damageCooldown;
+
+
+  // Variables related to animation
+  Animator animator;
+  Vector2 moveDirection = new Vector2(1,0);
 
 
   // Start is called before the first frame update
@@ -29,24 +34,39 @@ public class PlayerController : MonoBehaviour
   {
      MoveAction.Enable();
      rigidbody2d = GetComponent<Rigidbody2D>();
+     animator = GetComponent<Animator>();
+
+
      currentHealth = maxHealth;
   }
+ 
+  // Update is called once per frame
+  void Update()
+  {
+     move = MoveAction.ReadValue<Vector2>();
 
-   // Update is called once per frame
-   void Update()
-   {
-      move = MoveAction.ReadValue<Vector2>();
-      if (isInvincible)
-     { damageCooldown -= Time.deltaTime;
-       if (damageCooldown < 0)
+
+      if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y,0.0f))
+        {
+           moveDirection.Set(move.x, move.y);
+           moveDirection.Normalize();
+        }
+
+
+     animator.SetFloat("Look X", moveDirection.x);
+     animator.SetFloat("Look Y", moveDirection.y);
+     animator.SetFloat("Speed", move.magnitude);
+
+
+     if (isInvincible)
        {
-          isInvincible = false;
+           damageCooldown -= Time.deltaTime;
+           if (damageCooldown < 0)
+               isInvincible = false;
        }
-     }
-  }
+   }
 
-
-  // FixedUpdate has the same call rate as the physics system
+// FixedUpdate has the same call rate as the physics system
   void FixedUpdate()
   {
      Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
@@ -54,21 +74,21 @@ public class PlayerController : MonoBehaviour
   }
 
 
-   public void ChangeHealth(int amount)
-   {
-      if (amount < 0)
-      {
-         if (isInvincible)
-         {
-            return;
-         }
-         isInvincible = true;
-         damageCooldown = timeInvincible;
+  public void ChangeHealth (int amount)
+  {
+     if (amount < 0)
+       {
+           if (isInvincible)
+               return;
+          
+           isInvincible = true;
+           damageCooldown = timeInvincible;
+           animator.SetTrigger("Hit");
+       }
 
-      }
-      currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-     UIHandle.instance.SetHealthValue(currentHealth / (float)maxHealth);
+
+     currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+     UIHandler.instance.SetHealthValue(currentHealth / (float)maxHealth);
   }
-
 
 }
